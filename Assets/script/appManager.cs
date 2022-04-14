da50d70d80e519a9d050b6c41324226c099fe2f6
bonisantry <bonisantry@gmail.com>
2022-04-14 20:56:56 +0800

    APP

---
+++ Assets/script/appManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] backgroundDiffHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
